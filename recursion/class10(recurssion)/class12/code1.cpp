df2718688ead0f78b1977104409837a87152a63e
AshutoshDash-svg <ashutosh.2006.dash@gmail.com>
2026-08-21 09:23:35 +0530

    just one

--- recursion/class10(recurssion)/class12/code1.cpp
+++ recursion/class10(recurssion)/class12/code1.cpp
@@ -77,3 +77,31 @@ int main() {
 }
 
 
+//check whether a string is palindrom or not
+//ex=madam=madam
+#include<iostream>
+using namespace std;
+bool ispalindrome(string str,int i){
+    int n=str.length();
+    if(i>=n/2){
+        return true;
+    }
+    if(str[i]!=str[n-i-1]){
+        return false;
+    }
+    ispalindrome(str,i+1);
+}
+int main() {
+    string str;
+
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (ispalindrome(str, 0)) {
+        cout << "Palindrome";
+    } else {
+        cout << "Not Palindrome";
+    }
+
+    return 0;
+}
--- recursion/class10(recurssion)/class12/tempCodeRunnerFile.cpp
+++ recursion/class10(recurssion)/class12/tempCodeRunnerFile.cpp
@@ -1,27 +1,28 @@
-//if we take the arr from user (only for self improvement)
-#include <iostream>
+//check whether a string is palindrom or not
+//ex=madam=madam
+#include<iostream>
 using namespace std;
-void reverseArray(int arr[], int left, int right) {
-    if (left >= right) {
-        return;
+bool ispalindrome(string str,int i){
+    int n=str.length();
+    if(i>=n/2){
+        return true;
     }
-    swap(arr[left], arr[right]);
-    reverseArray(arr, left + 1, right - 1);
+    if(str[i]!=str[n-i-1]){
+        return false;
+    }
+    ispalindrome(str,i+1);
 }
 int main() {
-    int n;
-    cout << "Enter size of array: ";
-    cin >> n;
-    int arr[n];
-     cout << "Enter array elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    reverseArray(arr, 0, n - 1);
+    string str;
 
-    cout << "Reversed array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (ispalindrome(str, 0)) {
+        cout << "Palindrome";
+    } else {
+        cout << "Not Palindrome";
     }
+
     return 0;
 }
