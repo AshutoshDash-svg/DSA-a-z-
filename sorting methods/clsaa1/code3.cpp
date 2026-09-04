7bac10159e6c6ba5298dff86a1952155fc43d74a
AshutoshDash-svg <ashutosh.2006.dash@gmail.com>
2026-09-04 23:03:38 +0530

    insertion sort

--- sorting methods/clsaa1/code3.cpp
+++ sorting methods/clsaa1/code3.cpp
@@ -3,5 +3,25 @@
 #include<vector>
 using namespace std;
 void insertionsort(vector<int>& arr ,int n){
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
 
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    insertionsort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
 }
